5cdd9f60544ced11c740d69cd3e918f80c567096
dlouise64 <dana.iti.work@gmail.com>
2019-12-06 01:35:11 +1300

    add a msg on landscape

--- src/App.tsx
+++ src/App.tsx
@@ -11,13 +11,8 @@ export const AppContext: React.Context<any> = React.createContext(initialState)
 const App: React.FC = () => {
 	return (
 		<AppContext.Provider value={{ cards: cards }}>
-			<div
-				style={{
-					position: 'absolute',
-					bottom: 0,
-					left: 0,
-					right: 0
-				}}>
+			<div className="main">
+				<h1>Oops, this game is not built for landscape</h1>
 				<Board />
 			</div>
 		</AppContext.Provider>

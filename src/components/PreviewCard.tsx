252c2d8b19b0e786d7a8b8292e69edbe870f9b26
dlouise64 <dana.iti.work@gmail.com>
2019-12-08 01:17:12 +1300

    add card header text

--- src/components/PreviewCard.tsx
+++ src/components/PreviewCard.tsx
@@ -4,6 +4,7 @@ import styled from 'styled-components'
 interface IPreviewCard {
 	card?: {
 		color?: string
+		name?: string
 	}
 }
 const PreviewCard: React.FC<IPreviewCard> = props => (
@@ -11,9 +12,9 @@ const PreviewCard: React.FC<IPreviewCard> = props => (
 		{console.log('here', props.card && props.card.color)}
 		<svg viewBox="-175 -175 893 842">
 			<defs>
-				<rect id="a1" width="535" height="519" rx="30" />
+				<rect id="c1" width="535" height="524" rx="30" />
 				<filter
-					id="c1"
+					id="b1"
 					width="200%"
 					height="200%"
 					x="-50%"
@@ -25,41 +26,39 @@ const PreviewCard: React.FC<IPreviewCard> = props => (
 						in="shadowOffsetOuter1"
 						result="shadowBlurOuter1"
 					/>
-					<feComposite
-						in="shadowBlurOuter1"
-						in2="SourceAlpha"
-						operator="out"
-						result="shadowBlurOuter1"
-					/>
 					<feColorMatrix
 						values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.5 0"
 						in="shadowBlurOuter1"
 					/>
 				</filter>
-				<mask id="d1" width="535" height="524" x="0" y="0" fill="white">
+				<rect id="a1" width="486" height="81" x="28" y="30" rx="20" />
+				<mask id="d1" width="486" height="81" x="0" y="0" fill="white">
 					<use href="#a1" />
-				</mask>
-				<rect id="b1" width="486" height="81" x="28" y="30" rx="20" />
-				<mask id="e1" width="486" height="81" x="0" y="0" fill="white">
-					<use href="#b1" />
 				</mask>
 			</defs>
 			<g fill="none" fill-rule="evenodd" transform="translate(4 2)">
-				<use fill="black" filter="url(#c1)" href="#a1" />
-				<use
-					fill="#FFFFFF"
-					stroke="#FFFFFF"
-					stroke-width="2"
-					mask="url(#d1)"
-					href="#a1"
-				/>
+				<use fill="black" filter="url(#b1)" href="#c1" />
+				<use fill="#FFFFFF" href="#c1" />
 				<use
 					fill={props.card && props.card.color}
 					stroke={props.card && props.card.color}
 					stroke-width="2"
-					mask="url(#e1)"
-					href="#b1"
+					mask="url(#d1)"
+					href="#a1"
 				/>
+				<text
+					fill="#FFFFFF"
+					stroke="#000000"
+					stroke-width="2"
+					font-family="LilitaOne, Lilita One"
+					font-size="35"
+					letter-spacing="1.818"
+					textAnchor="middle"
+					alignmentBaseline="central">
+					<tspan x="30.5%" y="84">
+						{props.card && props.card.name && props.card.name.toUpperCase()}
+					</tspan>
+				</text>
 			</g>
 		</svg>
 	</PreviewCardWrapper>
